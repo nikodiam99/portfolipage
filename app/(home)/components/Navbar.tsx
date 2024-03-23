ca7b2c5b71d2408651adcca34943fa002cb0d474
nikodiam99 <nikodiam99@gmail.com>
2024-03-22 21:15:01 -0400

    Created and completed navbar

---
+++ app/(home)/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import React from 'react'
+import { SiGithub, SiLinkedin } from "react-icons/si";
+
+
+
+export default function Navbar() {
+    const socials = [
+        {
+            link: "https://www.linkedin.com/in/niko-diamantakis-70765117b/",
+
+            label: "Linkedin",
+
+            Icon:SiLinkedin,
+        },
+        {
+            link: "https://github.com/nikodiam99",
+
+            label: "Github",
+
+            Icon: SiGithub,
+
+        }
+    ]
+  
+    return (
+    <nav className='py-10 flex justify-between items-center px-10'>
+        <h1 className='text-2xl font-bold underline underline-offset-8 -rotate-2 decoration-cyan-700'>
+            Niko🪼
+        </h1>
+
+        <div className='flex items-center gap-5'>
+            {socials.map((social,index)=>{
+                const Icon = social.Icon
+                return <Link href={social.link} key={index} aria-label={social.label}>
+                    <Icon className='w-5 h-5 hover:scale-125 transition-all' />
+                </Link>
+            })}
+        </div>
+    </nav>
+  )
+}
---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+export default function page() {
+  return (
+    <div className='min-h-screen bg-black'>
+      <div className="max-w-7xl mx-auto"></div>
+      
+      <Navbar/>
+
+    </div>
+  )
+}
